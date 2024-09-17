d2248ac593c26984343c0e8a1d9201d6200a39fa
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-09-17 18:43:10 -0230

    Laying out basic structure

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const process = require("node:process");
+
+const arguments = process.argv;
+// console.log(arguments);
+
+function generatePassword {
+    let password = "";
+    return password;
+}
+
+console.log(`Your secure password: ${password}`);
