5e14f48bb7e395ae59c075f1ad34b2f7c1cec162
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-09-17 19:01:52 -0230

    Basic code expanded. Non-functional for now.

--- index.js
+++ index.js
@@ -2,12 +2,36 @@
 
 const process = require("node:process");
 
-const arguments = process.argv;
-// console.log(arguments);
+//Help Message
+function printHelpMsg() {
+  console.log(
+    `To use this app, please enter: 'generatepassword <Number of Characters for Password>'. 
 
-function generatePassword {
-    let password = "";
-    return password;
+    Example: generatepassword <9>
+    Expected output: Your secure password: xxxxxxxxx`
+  );
 }
 
+//Collect input from command line
+const passwordLength = process.argv.slice(2);
+
+//Required Variables
+let characters = [abcdefghijklmnopqrstuvwxyz];
+
+//Main password generation function
+function generatePassword() {
+  let password = "";
+  return password;
+}
+
+//Errors
+if (passwordLength.length < 8) {
+  printHelpMsg();
+  return;
+}
+
+//Call Function/Generate Password
+const password = generatePassword();
+
+//Print password output to console
 console.log(`Your secure password: ${password}`);
